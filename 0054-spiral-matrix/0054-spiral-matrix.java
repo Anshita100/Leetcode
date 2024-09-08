e0b9e3399252290b010fe15555bca6695a8b4730
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-09-08 13:21:47 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (46.93%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,36 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int startrow=0,endrow=matrix.length-1,startcol=0,endcol=matrix[0].length-1;
-        List<Integer> mylist=new ArrayList<Integer>();
-        while(startrow<=endrow && startcol<=endcol)
-        {
-            //top
-            for(int i=startcol;i<=endcol;i++)
-            {
-                mylist.add(matrix[startrow][i]);
+       
+        int startRow = 0;
+        int endRow = matrix.length - 1;
+        int startCol = 0;
+        int endCol = matrix[0].length - 1;
+
+        List<Integer> response = new ArrayList<>();
+
+        while (startRow <= endRow && startCol <= endCol) {
+            // top
+            for (int j = startCol; j <= endCol; j++) {
+                response.add(matrix[startRow][j]);
             }
-            //right
-            for(int i=startrow+1;i<=endrow;i++)
-            {
-                mylist.add(matrix[i][endcol]);
+
+            // right
+            for (int i = startRow + 1; i <= endRow; i++) {
+                response.add(matrix[i][endCol]);
             }
-            //bottom
-            for(int i=endcol-1;i>=startcol;i--)
-            {
-                if(startrow==endrow) break;
-                mylist.add(matrix[endrow][i]);
+
+            // bottom
+            if (startRow < endRow) { // Check to prevent duplicate row processing
+                for (int j = endCol - 1; j >= startCol; j--) {
+                    response.add(matrix[endRow][j]);
+                }
             }
-            //left
-            for(int i=endrow-1;i>startrow;i--)
-            {
-                if(startcol==endcol) break;
-                mylist.add(matrix[i][startcol]);
+
+            // left
+            if (startCol < endCol) { // Check to prevent duplicate column processing
+                for (int i = endRow - 1; i >= startRow + 1; i--) {
+                    response.add(matrix[i][startCol]);
+                }
             }
-            startrow++;
-            startcol++;
-            endcol--;
-            endrow--;
+
+            // Move boundaries inward
+            startRow++;
+            startCol++;
+            endRow--;
+            endCol--;
         }
-        return mylist;
+        return response;
     }
 }
