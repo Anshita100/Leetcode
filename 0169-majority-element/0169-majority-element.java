fff0ddc86ad27782993e444b35f9117ad2d7cdb1
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-09-21 22:38:03 +0530

    Time: 1 ms (99.79%), Space: 53.2 MB (29.97%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int majorityElement(int[] nums) {
+  /*  public int majorityElement(int[] nums) {
         int n=nums.length;
         int a=(n/2)+1;
         
@@ -12,5 +12,19 @@ class Solution {
                 return i;
         }
         return -1;
+    }*/
+    public int majorityElement(int[] num) {
+
+        int major=num[0], count = 1;
+        for(int i=1; i<num.length;i++){
+            if(count==0){
+                count++;
+                major=num[i];
+            }else if(major==num[i]){
+                count++;
+            }else count--;
+            
+        }
+        return major;
     }
 }
