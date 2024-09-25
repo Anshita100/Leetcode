e6200d763209e9fec2b535ebd156fd0fedc981ee
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-09-25 18:29:28 +0530

    Time: 34 ms (38.26%), Space: 52.4 MB (22.63%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,31 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);  // Sort the array
         
         for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicates for the first number
+            // Skip duplicates for the first element
             if (i > 0 && nums[i] == nums[i - 1]) continue;
-            
+
             int j = i + 1;
             int k = nums.length - 1;
-            
+
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
                 
                 if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    
-                    // Move the pointers and skip duplicates
+                    List<Integer> ans = new ArrayList<>();
+                    ans.add(nums[i]);
+                    ans.add(nums[j]);
+                    ans.add(nums[k]);
+                    result.add(ans);
+
+                    // Skip duplicates for the second element
                     while (j < k && nums[j] == nums[j + 1]) j++;
+                    // Skip duplicates for the third element
                     while (j < k && nums[k] == nums[k - 1]) k--;
                     
+                    // Move pointers after finding a valid triplet
                     j++;
                     k--;
                 } else if (sum < 0) {
@@ -31,7 +35,6 @@ class Solution {
                 }
             }
         }
-        
         return result;
     }
 }
