80623e6d79460d882508938c463a3d2af622efdd
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-10-07 09:15:15 +0530

    Time: 2 ms (97.04%), Space: 43 MB (82.48%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -1,6 +1,6 @@
 class Solution {
   
-    public int minLength(String s) {
+   /* public int minLength(String s) {
     
        Stack<Character> st=new Stack<>();
       st.push(s.charAt(0));
@@ -19,43 +19,30 @@ class Solution {
           }
       }
       return st.size();
-    }
-    
-    /* public int minLength(String s) {
-        Stack<Character> st = new Stack<>();
-        int n = s.length();
-        st.push(s.charAt(0));
-        for(int i=1;i<n;i++)
-        {
-            if(!st.isEmpty()&&((s.charAt(i)=='B'&&st.peek()=='A')||(s.charAt(i)=='D'&&st.peek()=='C')))
-            {
-                st.pop();
-            }
-            else
-                st.push(s.charAt(i));
-        }
-        return st.size();
     }*/
+    
+    public int minLength(String s) {
+    int left = 0;
+    StringBuilder result = new StringBuilder();
+    while (left < s.length()) {
+        char ch = s.charAt(left);
+        int len = result.length();
+        if (len > 0 && ((ch == 'B' && result.charAt(len - 1) == 'A') || (ch == 'D' && result.charAt(len - 1) == 'C'))) {
+            result.deleteCharAt(len - 1); // Remove the last character
+        } else {
+            result.append(ch);
+        }
+        left++;
+    }
+    return result.length();
+}
+
     /*public int minLength(String s) {
       while (s.contains("AB") || s.contains("CD")) {
         s = s.replaceAll("AB", "").replaceAll("CD", "");
        }
       return s.length();
     }*/
-   /* public int minLength(String s) {
-        Stack st = new Stack<>();
-     for(char ch : s.toCharArray()) {
-    if(st.size() > 0) {
-    if((ch == 'B' && st.peek() == 'A') || (ch == 'D'                 && st.peek() == 'C')) {
-              st.pop();
-    } else {
-             st.push(ch);
-            }
-    }
-     else {
-       st.push(ch);
-      }
-    }
-return st.size();
-}*/
+    
+   
 }
