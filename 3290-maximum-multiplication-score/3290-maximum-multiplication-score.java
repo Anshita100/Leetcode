2c8dc8ec84ab6104e25553f84df992b56fb6a3a0
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-10-08 11:10:12 +0530

    Time: 8 ms (83.58%), Space: 61.2 MB (81.76%) - LeetHub

--- 3290-maximum-multiplication-score/3290-maximum-multiplication-score.java
+++ 3290-maximum-multiplication-score/3290-maximum-multiplication-score.java
@@ -3,12 +3,12 @@ class Solution {
        long [][] dp = new long[5][b.length + 1];
        for (int i = 1; i <= 4; i++) {
            for (int j = i; j <= b.length; j++) {
-               long take = dp[i-1][j-1] + (long)a[i-1] * b[j-1];
-               long notTake = dp[i][j-1];
+               long pick = dp[i-1][j-1] + (long)a[i-1] * b[j-1];
+               long unpick = dp[i][j-1];
                if (j - 1 < i) {
-                   dp[i][j] = take;
+                   dp[i][j] = pick;
                } else {
-                   dp[i][j] = Math.max(take, notTake);
+                   dp[i][j] = Math.max(pick, unpick);
                }
            }
        }
