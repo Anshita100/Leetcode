1f85712ed06a36ceb2c979563d884e440addbde6
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-10-11 00:41:11 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (8.00%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,17 +1,23 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        // Call the isInvert method directly
-        return isInvert(root.left, root.right);
+        return isInvert(root.left, root.right) || isSame(root);
     }
 
     public boolean isInvert(TreeNode left, TreeNode right) {
-        // If both nodes are null, they are mirrors
         if (left == null && right == null)
             return true;
-        // If one of them is null, they are not mirrors
         if (left == null || right == null)
             return false;
-        // Check if values are the same and subtrees are mirrors
         return (left.val == right.val) && isInvert(left.left, right.right) && isInvert(left.right, right.left);
+    }
+
+    public boolean isSame(TreeNode root) {
+        if (root == null)
+            return true;
+        if ((root.left == null && root.right != null) || (root.left != null && root.right == null))
+            return false;
+        if (root.right != null && root.left != null && root.left.val != root.right.val)
+            return false;
+        return isSame(root.left) && isSame(root.right);
     }
 }
