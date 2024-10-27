b7ccc0f2fbe636601fcf99e8bc99a3949a365b9d
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-10-27 10:50:00 +0530

    Time: 1 ms (100.00%), Space: 42 MB (100.00%) - LeetHub

--- 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.java
+++ 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.java
@@ -2,18 +2,22 @@ class Solution {
     public int possibleStringCount(String word) {
         int totalCount = 1;
         int i = 0;
-        
+
         while (i < word.length()) {
             int j = i;
             while (j < word.length() && word.charAt(j) == word.charAt(i)) {
                 j++;
             }
-            
+
             int segmentLength = j - i;
             totalCount += segmentLength - 1;
             i = j;
         }
-        
+
         return totalCount;
     }
 }
+
+/*Iterate through the string to find segments of consecutive identical characters.
+For each segment, calculate the number of original strings that could lead to this segment by adding segmentLength - 1 to the total count.
+Continue until the end of the string and return the total count.*/
