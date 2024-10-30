214eddc4453978b4d0b5769d1e7a416354668603
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-10-30 09:15:48 +0530

    Time: 33 ms (37.87%), Space: 52.6 MB (15.79%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -29,15 +29,19 @@ class Solution {
                     ans.add(nums[j]);
                     ans.add(nums[k]);
                     result.add(ans);
+                    
+                    
 
                     // Skip duplicates for the second element
                     while (j < k && nums[j] == nums[j + 1]) j++;
                     // Skip duplicates for the third element
                     while (j < k && nums[k] == nums[k - 1]) k--;
                     
-                    // Move pointers after finding a valid triplet
+                     // Move pointers after finding a valid triplet
                     j++;
                     k--;
+                    
+                   
                 } else if (sum < 0) {
                     j++;
                 } else {
@@ -48,3 +52,33 @@ class Solution {
         return result;
     }
 }
+
+
+//more time complexity not so good approach
+/*   public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            Set<Integer> seen = new HashSet<>();
+            for (int j = i + 1; j < nums.length; j++) {  // Ensure j starts after i
+                int complement = - (nums[i] + nums[j]);
+                
+                // Ensure nums[j] != complement, and we avoid adding nums[i] == nums[j]
+                if (seen.contains(complement)) {
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
+                    Collections.sort(triplet);  // Sort triplet to avoid duplicates
+                    result.add(triplet);  // Add the triplet to result set
+                }
+                
+                seen.add(nums[j]);  // Add current number to HashSet
+            }
+        }
+        return new ArrayList<>(result);
+    } 
+
+*/
+
+
+
+
+
