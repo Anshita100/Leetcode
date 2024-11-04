4bb4bbac70d0f8a9befeb500754f93f45720c8d1
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-11-04 17:47:41 +0530

    Time: 24 ms (89.83%), Space: 62.9 MB (62.43%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maximumCandies(int[] candies, long k) {
+        int start = 1;
+        int end = Integer.MIN_VALUE;
+
+        // Find the maximum value in the candies array
+        for (int candy : candies) {
+            if (candy > end) {
+                end = candy;
+            }
+        }
+
+        int ans = 0;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (canAllocate(candies, mid, k)) {
+                ans = mid;  // mid is a valid answer, try for a larger value
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public boolean canAllocate(int[] candies, int candy, long child) {
+        long currentChild = 0;
+        for (int i = 0; i < candies.length; i++) {
+            currentChild += candies[i] / candy;
+            if (currentChild >= child) {  // Early exit if we meet or exceed required children
+                return true;
+            }
+        }
+        return currentChild >= child;
+    }
+}
