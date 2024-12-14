c76b7d0e535ed62e7b1e7391596060275138731c
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-12-14 13:00:03 +0530

    Time: 3 ms (100.00%), Space: 76.9 MB (66.60%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -17,4 +17,25 @@ class Solution {
         }
         return temp;
     }
+  /* its not passing all the testcases so leave this method
+  public int[] rearrangeArray(int[] nums) {
+        int pos=0,neg=1; int n=nums.length;
+        while(pos<n && neg<n)
+        {
+            if(nums[pos]>=0) pos=pos+2;
+            else if(nums[neg]<0) neg=neg+2;
+            else//pos wala negative value ko point kar raha hai aur neg wala positve value ko toh swap kardo
+            {
+                int temp=nums[pos];
+                nums[pos]=nums[neg];
+                nums[neg]=temp;
+                
+                pos=pos+2;
+                neg=neg+2;
+            }    
+        }
+        return nums;
+    }*/
+  
+
 }
