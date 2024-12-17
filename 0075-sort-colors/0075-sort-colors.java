245c8cfcb4cfefb0b0c707bd3c4eb4666e18674b
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-12-17 23:49:20 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (16.46%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,6 +1,21 @@
 class Solution {
     public void sortColors(int[] nums) {
         //brute force approach
-        Arrays.sort(nums);
+        //Arrays.sort(nums); TC-O(n*logn)
+        
+        //better approach
+        int a=0,b=0,c=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==0) a++;
+            else if(nums[i]==1) b++;
+            else
+            c++;
+        }
+        int j=0;
+        for(j=0;j<a;j++) nums[j]=0;
+        for(j=a;j<(a+b);j++) nums[j]=1;
+        for(j=(a+b);j<(a+b+c);j++) nums[j]=2;
+        
     }
 }
