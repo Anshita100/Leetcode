7d69b2e77842cf61590acf9479003349b43745c8
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-12-22 17:37:47 +0530

    Time: 5 ms (49.44%), Space: 42.2 MB (55.12%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String removeOuterParentheses(String s) {
+  /*  public String removeOuterParentheses(String s) {
         StringBuilder sb=new StringBuilder();
         int opened=0;
         for(char c:s.toCharArray())
@@ -16,6 +16,28 @@ class Solution {
                 
                 opened--;
             }    
+        }
+        return sb.toString();
+    }*/
+    public String removeOuterParentheses(String s) {
+        StringBuilder sb=new StringBuilder();
+        Stack<Character>st=new Stack<>();
+        
+        for(char c:s.toCharArray())
+        {
+            if(c=='(')
+            {
+                if(!st.isEmpty())
+                    sb.append(c);
+                
+                st.push(c);
+            }
+            else
+            {
+                st.pop();
+                if(!st.isEmpty())
+                    sb.append(c);          
+            }
         }
         return sb.toString();
     }
