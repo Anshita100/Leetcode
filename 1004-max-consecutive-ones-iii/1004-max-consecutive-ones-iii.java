5ce4317a793415260a86a61d7ffc084c518dc923
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-12-25 20:11:44 +0530

    Time: 3 ms (86.77%), Space: 46.9 MB (32.94%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,7 +1,8 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
       //longest subarray with atmost k zeroes
-        int n=nums.length;int maxlen=0;
+       //brute force 
+       /* int n=nums.length;int maxlen=0;
         for(int i=0;i<n;i++)
         {
             int count=0;
@@ -15,6 +16,37 @@ class Solution {
                else//count>k
                break;    
             }
+        }
+        return maxlen;*/
+        //sliding window
+        int l=0,r=0;int count=0;int maxlen=0;
+        while(r<nums.length)
+        {
+           if(nums[r]==1)
+           {
+               
+               maxlen=Math.max(maxlen,r-l+1);
+               r++;
+           }
+           else //nums[r]==0
+           {
+               count++;
+               if(count>k)
+               {
+                   while(l<=r && nums[l]==1) l++;
+                   
+                   //nums[l]=0
+                   l++; count--;
+                   maxlen=Math.max(maxlen,r-l+1);
+                   r++;
+               }
+               else
+               {
+                   maxlen=Math.max(maxlen,r-l+1);
+                   r++;
+               }    
+           }    
+                
         }
         return maxlen;
     }
