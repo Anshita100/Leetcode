f2d33515fc3233b64f7ca2953c83f4faf8c4663d
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-12-25 20:29:43 +0530

    Time: 3 ms (86.77%), Space: 47.1 MB (25.60%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -19,12 +19,11 @@ class Solution {
         }
         return maxlen;*/
         //sliding window
-        int l=0,r=0;int count=0;int maxlen=0;
-        while(r<nums.length)
+      /*  int l=0,r=0;int count=0;int maxlen=0;
+        while(r<nums.length)//O(n)
         {
            if(nums[r]==1)
            {
-               
                maxlen=Math.max(maxlen,r-l+1);
                r++;
            }
@@ -33,8 +32,7 @@ class Solution {
                count++;
                if(count>k)
                {
-                   while(l<=r && nums[l]==1) l++;
-                   
+                   while(l<=r && nums[l]==1) l++;//O(n)
                    //nums[l]=0
                    l++; count--;
                    maxlen=Math.max(maxlen,r-l+1);
@@ -47,6 +45,25 @@ class Solution {
                }    
            }    
                 
+        }
+        return maxlen;//TC-O(2n)*/
+        
+        int l=0,r=0,maxlen=0,count=0;
+        for(r=0;r<nums.length;r++)
+        {
+            if(nums[r]==0) count++; 
+            
+            if(count>k)
+            {
+                if(nums[l]==0) 
+                    count--; 
+                
+                l++;  
+            }
+            if(count<=k)
+            {
+                maxlen=Math.max(maxlen,r-l+1); 
+            }
         }
         return maxlen;
     }
