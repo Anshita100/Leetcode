2f7f494e95f8e8feec41cfaa6c9b2fcd6cbdbbc1
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-12-25 21:56:20 +0530

    Time: 2 ms (65.58%), Space: 47.6 MB (68.19%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int numSubarraysWithSum(int[] a, int goal) {
+    //brute approach TC-O(n*n)
+  /*  public int numSubarraysWithSum(int[] a, int goal) {
         int ans=0;
         int n=a.length;
         for(int i=0;i<n;i++)
@@ -12,6 +13,26 @@ class Solution {
                         ans++;
                 
             }
+        }
+        return ans;        
+    }*/
+    
+    public int numSubarraysWithSum(int[] a, int goal) {
+        return count(a,goal)-count(a,goal-1);
+    }
+    public static int count(int[]a ,int goal)//count subarray which have sum less than or equal to goal
+    {
+        if(goal<0) return 0;
+        int l=0,r=0,ans=0,sum=0;
+        for(r=0;r<a.length;r++)
+        {
+            sum=sum+a[r];
+           
+            while(sum>goal)//shink
+            { 
+              sum=sum-a[l]; l++;  
+            }
+            ans=ans+(r-l+1);
         }
         return ans;
     }
