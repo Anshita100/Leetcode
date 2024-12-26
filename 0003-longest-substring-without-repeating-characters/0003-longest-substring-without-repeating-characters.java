c547b26e192480ee4aff145f0371a597ed6e4c18
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-12-26 23:23:58 +0530

    Time: 6 ms (70.18%), Space: 44.9 MB (26.03%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -9,12 +9,12 @@ Space Complexity: O(N) where N is the size of HashSet taken for storing the elem
         int ans=Integer.MIN_VALUE;
         for(int i=0;i<n;i++)
         {
-             HashSet<Character>hs=new HashSet<>();
+              Set<Character>hs=new HashSet<>();
               for(int j=i;j<n;j++)
               {
                   if(hs.contains(s.charAt(j)))
                   {
-                      ans=Math.max(ans,j-i);
+                      ans=Math.max(ans,(j-1)-i+1);
                       break;
                   }
                    hs.add(s.charAt(j));
@@ -23,30 +23,39 @@ Space Complexity: O(N) where N is the size of HashSet taken for storing the elem
         return ans;
     }*/
     
+    //optimised approach
     public int lengthOfLongestSubstring(String s) {
-        
         int l=0;int ans=0;
-        HashSet<Character>hs=new HashSet<>();
+        Set<Character>hs=new HashSet<>();
         for(int r=0;r<s.length();r++)
         {
            
-            if(hs.contains(s.charAt(r))==false)
-            {
-                hs.add(s.charAt(r));
-                ans=Math.max(ans,r-l+1);
-            }
-            else
-            {
-                while(l<r && hs.contains(s.charAt(r)))
-                {
+                while (hs.contains(s.charAt(r))) {
                     hs.remove(s.charAt(l));
                     l++;
                 }
                 hs.add(s.charAt(r));
                 ans=Math.max(ans,r-l+1);
-            }
         }
-        return ans;
-        
+        return ans;     
     }
+    
+/*     public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int l = 0, r = 0;
+        int maxlen = 0;
+        HashMap<Character, Integer> hm = new HashMap<>();
+
+        while (r < n) {
+            char currentChar = s.charAt(r);
+            if (hm.containsKey(currentChar)) {
+                l = Math.max(hm.get(currentChar) + 1, l);  // Ensure 'l' only moves forward
+            }
+            hm.put(currentChar, r);
+            maxlen = Math.max(maxlen, r - l + 1);
+            r++;
+        }
+
+        return maxlen;
+    }*/
 }
