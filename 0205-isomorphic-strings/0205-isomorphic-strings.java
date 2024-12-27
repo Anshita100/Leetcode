3b98d454f75b57d12c34f07ae6dbe1e0c68d5d7b
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2024-12-27 14:36:27 +0530

    Time: 17 ms (38.29%), Space: 42.7 MB (28.00%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap <Character,Character> map=new HashMap<>();
+     /*   HashMap <Character,Character> map=new HashMap<>();
         
         int n=s.length();
         int m=t.length();
@@ -10,21 +10,40 @@ class Solution {
 for (int i = 0; i < s.length(); i++) {
             char charS = s.charAt(i);
             char charT = t.charAt(i);
-
-            if (map.containsKey(charS)) {
-                // Check if the current mapping is consistent
+            if (map.containsKey(charS)) {//eg-s=foo,t=bar
                 if (map.get(charS) != charT) {
-                    return false; // Conflict in mapping
+                    return false; 
                 }
             } else {
-                // Check if charT is already mapped to some other charS
-                if (map.containsValue(charT)) {
-                    return false; // Conflict in reverse mapping
+               if (map.containsValue(charT)) {//eg-s=bad,t=bab
+                    return false; 
                 }
-                // Add the mapping from charS to charT
                 map.put(charS, charT);
             }
         }
+        return true;*/
+        if (s.length() != t.length()) return false;
+        
+        HashMap<Character, Character> mapS = new HashMap<>();
+        HashMap<Character, Character> mapT = new HashMap<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            
+            if (mapS.containsKey(ch1)) {
+                if (mapS.get(ch1) != ch2) return false;
+            } else {
+                mapS.put(ch1, ch2);
+            }
+            
+            if (mapT.containsKey(ch2)) {
+                if (mapT.get(ch2) != ch1) return false;
+            } else {
+                mapT.put(ch2, ch1);
+            }
+        }
+        
         return true;
     }
 }
