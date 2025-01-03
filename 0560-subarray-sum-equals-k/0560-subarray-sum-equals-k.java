ef0fcc13758a082cdf0988e56cd531676b5f99c0
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2025-01-03 23:44:54 +0530

    Time: 24 ms (73.6%), Space: 46.9 MB (17.92%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int subarraySum(int[] nums, int k) {
+    //brute force approach-har ek subarray ka sum calculate karo aur check karo ki k ke value hai kya?
+   /*public int subarraySum(int[] nums, int k) {
         int n=nums.length;
         int count=0;
         for(int i=0;i<n;i++)
@@ -13,5 +14,44 @@ class Solution {
            }
         }
         return count;
-    }
+    }*/
+    /*public int subarraySum(int[] nums, int k) {
+        return ans(nums,k)-ans(nums,k-1);
+    } 
+    public int ans(int[] nums, int k)
+    {
+        if(k<=0) return 0;
+        //count subarray whose sum is less than or equal to k
+        int l=0;int sum=0;int count=0;
+        for(int r=0;r<nums.length;r++)
+        {
+             sum=sum+nums[r];
+             while(sum>k)
+             {
+                 sum=sum-nums[l]; l++;
+             }
+            
+                count=count+(r-l+1);
+             
+        }
+        return count;
+    }  */
+    public int subarraySum(int[] nums, int k) {
+     HashMap<Integer, Integer> subNum = new HashMap<>();
+        subNum.put(0, 1);
+        int total = 0, count = 0;
+
+        for (int n : nums) {
+            total += n;
+
+            if (subNum.containsKey(total - k)) {
+                count += subNum.get(total - k);
+            }
+
+            subNum.put(total, subNum.getOrDefault(total, 0) + 1);
+        }
+
+        return count;        
+    } 
+    
 }
