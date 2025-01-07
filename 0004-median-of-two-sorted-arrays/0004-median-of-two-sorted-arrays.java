b7b61af2cbcabf74d87cfb524c6ac309539fb47a
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2025-01-07 21:29:29 +0530

    Time: 1 ms (100%), Space: 46 MB (75.48%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,6 +1,7 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n=nums1.length,m=nums2.length;
+       //brute approach
+        /*int n=nums1.length,m=nums2.length;
         int a[]=new int[n+m];
         int i=0,j=0,k=0;
         while(i<n && j<m)
@@ -16,7 +17,67 @@ class Solution {
         if(a.length%2==0)
            return (a[a.length / 2] + a[(a.length / 2) - 1]) / 2.0;
         else
-        return a[a.length/2];
+        return a[a.length/2];*/
+        
+    int i = 0, j = 0;
+    int k = nums1.length + nums2.length;
+
+    // Median positions
+    int idx1 = (k - 1) / 2;
+    int idx2 = k / 2;
+
+    // Variables to hold values at idx1 and idx2
+    int f = -1, e = -1;
+
+    // Counter for the merged array
+    int count = -1;
+
+    // Merge process
+    while (i < nums1.length && j < nums2.length) {
+        count++;
+        int current;
+        if (nums1[i] < nums2[j]) {
+            current = nums1[i];
+            i++;
+        } else {
+            current = nums2[j];
+            j++;
+        }
+
+        // Update f and e for median positions
+        if (count == idx1) f = current;
+        if (count == idx2) e = current;
         
     }
-}
+
+    // Handle remaining elements in nums1
+    while (i < nums1.length) {
+        count++;
+        int current = nums1[i];
+        i++;
+
+        // Update f and e for median positions
+        if (count == idx1) f = current;
+        if (count == idx2) e = current;
+        
+    }
+
+    // Handle remaining elements in nums2
+    while (j < nums2.length) {
+        count++;
+        int current = nums2[j];
+        j++;
+
+        // Update f and e for median positions
+        if (count == idx1) f = current;
+        if (count == idx2) e = current;
+        
+    }
+
+    // Return median
+    if (k % 2 == 0) {
+        return (f + e) / 2.0; // Even case
+    } else {
+        return e; // Odd case
+    }
+}}
