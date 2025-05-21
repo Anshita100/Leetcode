c6fdc18835976570e9671d38b66bf08c2591ef16
Anshita100 <128876959+Anshita100@users.noreply.github.com>
2025-05-21 15:05:45 +0530

    Time: 5 ms (45.55%), Space: 42.2 MB (66.25%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,26 +1,63 @@
 class Solution {
-  /*  public String removeOuterParentheses(String s) {
+    //2ms
+    public String removeOuterParentheses(String s) {
         StringBuilder sb=new StringBuilder();
-        int opened=0;
+        int opened=0;Stack<Character>st=new Stack<>();
         for(char c:s.toCharArray())
         {
             if(c=='(')
             {
-                if(opened >0) sb.append(c);
-                
-                opened++;
+                opened++; st.push(c);
+                if(opened>1)
+                sb.append(c);
             }
             else
             {
-                if(opened >1) sb.append(c);
-                
                 opened--;
-            }    
+                if(opened==0)st.pop();
+                else
+                {
+                    st.pop();
+                    sb.append(c);
+                }
+            }
         }
         return sb.toString();
-    }*/
-    public String removeOuterParentheses(String s) {
-        StringBuilder sb=new StringBuilder();
+       /* StringBuilder sb=new StringBuilder();
+        int opened=0;
+        for(char c:s.toCharArray())
+        {
+            //my approach
+            if(c=='(')
+            {
+                opened++;
+              
+                //if(opened ==1) dont append
+                if(opened>1)
+                sb.append(c);
+                
+            }
+            else
+            {
+                opened--;
+
+                //if(opened==0) dont append
+                if(opened>0)
+                sb.append(c);           
+            } 
+        }
+        return sb.toString();*/
+    }
+    //5ms
+   // public String removeOuterParentheses(String s) {
+      /*  If the character is '(':
+If the stack is not empty, append it to the result (because it's not an outermost parenthesis).
+Push '(' onto the stack.
+If the character is ')':
+pop the top of the stack (to close the last opened '(').
+If the stack is not empty, append ')' to the result (because it's not an outermost parenthesis). 
+At last-The StringBuilder contains the modified string after removing the outermost parentheses.*///TC-O(N),SC-O(N)
+       /* StringBuilder sb=new StringBuilder();
         Stack<Character>st=new Stack<>();
         
         for(char c:s.toCharArray())
@@ -39,6 +76,6 @@ class Solution {
                     sb.append(c);          
             }
         }
-        return sb.toString();
-    }
+        return sb.toString();*/
+    //}
 }
